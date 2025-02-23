c559a51943387e6fa7aba14c607dff814afa2b26
AbrarAbe <abrar.abe01@gmail.com>
2025-02-23 10:19:54 +0700

    feat: add site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://abrarabe.github.io',
+	base: 'myhomepage',
 	vite: {
 		plugins: [tailwindcss()]
 	}
